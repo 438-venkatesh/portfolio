23b0b44a7bd585095d525fee656e2d87bd24545f
438-venkatesh <n210438@rguktn.ac.in>
2024-10-09 15:10:40 +0530

    Updated dependencies to latest versions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt"; // Change to a named import
 import { motion } from "framer-motion";
+
+// Your component code...
+
 
 import { styles } from "../styles";
 import { services } from "../constants";
